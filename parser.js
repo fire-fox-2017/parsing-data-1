60dd78196f4c95897056800de08bb26e8b2112f8
Bhirawa Mbani <bhirmbani@gmail.com>
2017-04-09 10:40:05 +0700

    parsing data 1 done

--- parser.js
+++ parser.js
@@ -11,7 +11,7 @@ class Person {
     this.lastName = profile.lastName;
     this.email = profile.email;
     this.phone = profile.phone;
-    this.createdAt = profile.createdAt || new Date();
+    this.createdAt = profile.createdAt || new Date().toISOString();
   }
 }
 
@@ -19,29 +19,64 @@ class PersonParser {
 
   constructor(file) {
     this._file = file
-    this._people = fs.readFileSync(this._file).toString().split("\n");
-    this._newPeople = [];
+    this._people = [];
   }
-
+  
+  // get people() sebagai read data people.csv
   get people() {
-    return this._people
+    let csvInArr = fs.readFileSync(this._file, 'utf-8').split("\n"); //arr of str
+    let personData; // idx arr => tiap property Person
+    for(let i = 1; i < csvInArr.length; i++) {
+      personData = csvInArr[i].split(',');
+      this._people.push(new Person({
+        'id': personData[0], 
+        'firstName': personData[1], 
+        'lastName': personData[2], 
+        'email': personData[3], 
+        'phone': personData[4], 
+        'createdAt': personData[5]
+      }));
+      // this._people => arr of obj Person
+    }
   }
-
-  addPerson(profile) {
-    let arrProfileData = [profile.id, profile.firstName, profile.lastName, profile.email, profile.phone, profile.createdAt];
-    // let arrToString = arrProfileData.toString();
-    this._newPeople.push(arrProfileData);
+  
+  addPerson(personData) {
+    this._people.push(new Person({
+      'id': this._people.length + 1,
+      'firstName': personData.firstName,
+      'lastName': personData.lastName,
+      'email': personData.email,
+      'phone': personData.phone,
+      'createdAt': personData.createdAt
+    }))
   }
 
   save() {
-    let rawPeopleDataInArr = [];
-    let newPeopleData = this._newPeople.join('\n');
-    fs.writeFileSync(this._file, newPeopleData, 'utf-8')
+
+    // this._people yg obj of Person jadiin string disini ya
+    for(let i = 0; i < this._people.length; i++) {
+      this._people[i] = `${this._people[i].id},${this._people[i].firstName},${this._people[i].lastName},${this._people[i].email},${this._people[i].phone},${this._people[i].createdAt}`;
+    }
+
+    // csv data beres
+    let csvWithRowName = "id,first_name,last_name,email,phone,created_at\n";
+    csvWithRowName += this._people.join('\n')
+
+    // data yg beres di write
+    fs.writeFile(this._file, csvWithRowName, 'utf-8', (err, data) => {
+      if(!err) {
+        return data;
+      } else {
+        console.log(err.message);
+      }
+    });
   }
 
 }
 
-let parser = new PersonParser('people.csv')
+let parser = new PersonParser('people.csv');
+
+parser.people
 
 parser.addPerson(new Person({
   'id': '201',
@@ -49,11 +84,20 @@ parser.addPerson(new Person({
   'lastName': 'Doni',
   'email': 'ahmaddoni@gmail.com',
   'phone': '1-222-111-9991',
-  'createdAt': '2012-06-18T15:32:40-07:00'
 }))
 
-// parser.save();
-console.log(parser.people[parser.people.length - 1])
-console.log(parser._newPeople.join('\n'))
+parser.addPerson(new Person({
+  'id': '202',
+  'firstName': 'Mark',
+  'lastName': 'Doni',
+  'email': 'mark@gmail.com',
+  'phone': '1-102-238-1282',
+}))
 
-console.log(`There are ${parser.people.length} people in the file '${parser._file}'.`)
+parser.save();
+
+
+
+
+
+console.log(`There are ${parser._people.length} people in the file '${parser._file}'.`)
