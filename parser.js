3676fe342085b1a1c3d9fa2cd5b2c5b51e2c6c69
arahito <uarahito@yahoo.com>
2017-04-10 03:44:04 +0700

    Parsing data 1

--- parser.js
+++ parser.js
@@ -1,25 +1,62 @@
 "use strict"
+let fs = require('fs')
 
 class Person {
-  // Look at the above CSV file
-  // What attributes should a Person object have?
+    // Look at the above CSV file
+    // What attributes should a Person object have?
+    constructor(component) {
+        this._id = component['id']
+        this._first_name = component['first_name']
+        this._last_name = component['last_name']
+        this._email = component['email']
+        this._phone = component['phone']
+        this._created_at = component['created_at']
+    }
+
+    get dataPeople() {
+        return `${this._id},${this._first_name},${this._last_name},${this._email},${this._phone},${this._created_at}`;
+    }
+
 }
 
 class PersonParser {
 
-  constructor(file) {
-    this._file = file
-    this._people = null
-  }
+    constructor(file) {
+        this._file = file
+        this._people = []
+    }
 
-  get people() {
-    return this._people
-  }
+    get people() {
+        let data_csv = fs.readFileSync(this._file).toString().split("\n")
+        for (let i = 0; i < data_csv.length; i++) {
+            this._people.push(data_csv[i])
+        }
+        return this._people
+    }
 
-  addPerson() {}
+    addPerson(objPerson) {
+        let person = new Person(objPerson)
+        this._people.push(person.dataPeople)
+    }
 
+    save() {
+        let saveFile = fs.writeFileSync(this._file, this._people.join('\n'))
+        return saveFile
+    }
+}
+
+let object = {
+    id: 201,
+    first_name: 'Uci',
+    last_name: 'Lubis',
+    email: 'arahitolubis@gmail.com',
+    phone: '454-5343',
+    created_at: new Date()
 }
 
 let parser = new PersonParser('people.csv')
+parser.people
+parser.addPerson(object)
+parser.save()
 
-console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+// console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
